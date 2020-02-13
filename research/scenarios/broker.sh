56fe4a83e2547e281ab625be5096f0f8c2521b33
Nick Gashkov <nick@gashkov.com>
2020-02-13 13:23:52 +0300

    Add wallets and scenarios to bootstrap the network

---
+++ research/scenarios/broker.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env sh
+
+set -e
+
+DAEMON_SOCKETNAME="${DAEMON_SOCKETNAME:-http://localhost:18236}"
+SIMPLEWALLET_HOST=0.0.0.0
+SIMPLEWALLET_PORT=19236
+
+/app/build/release/src/simplewallet \
+  --testnet \
+  --daemon-address="$DAEMON_SOCKETNAME" \
+  --wallet-file=/app/research/wallets/broker.bin.wallet \
+  --password=pass \
+  --rpc-bind-ip="$SIMPLEWALLET_HOST" \
+  --rpc-bind-port="$SIMPLEWALLET_PORT"
